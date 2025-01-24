66ee1c0a45046481e0d718b196e5a2d133092485
skcicd <skcicd@gmail.com>
2025-01-24 23:20:58 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,14 @@
+import unittest
+from src.app import app
 
+class TestFlaskApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello, DevSecOps!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
